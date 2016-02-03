f07d0c4fcd77a2cf0c15057a367ef04382d58be5
Rob Conklin <rob.conklin@proquest.com>
2016-02-03 14:12:35 -0500

    Added a hint

--- PhoneBookTestApp/PhoneBookTestApp/Program.cs
+++ PhoneBookTestApp/PhoneBookTestApp/Program.cs
@@ -9,12 +9,12 @@ namespace PhoneBookTestApp
 {
     class Program
     {
+        private PhoneBook phonebook = new PhoneBook();
         static void Main(string[] args)
         {
             try
             {
                 DatabaseUtil.initializeDatabase();
-
                 /* TODO: create person objects and put them in the PhoneBook and database
                 * John Smith, (248) 123-4567, 1234 Sand Hill Dr, Royal Oak, MI
                 * Cynthia Smith, (824) 128-8758, 875 Main St, Ann Arbor, MI
